d569ed48ecad97b3c110176ef968fe9122ea52f3
sakshinale41-creator <sakshinale41@gmail.com>
2026-09-22 22:18:53 +0530

    Add size choices and slug configuration to Product model

--- shop/models.py
+++ shop/models.py
@@ -21,11 +21,11 @@ class Product(models.Model):
     stock = models.PositiveIntegerField(default=10)
     created = models.DateTimeField(auto_now_add=True)
     SIZE_CHOICES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-        ('XL', 'Extra Large'),
-        ('XXL', 'Double XL'),
+        ('S', 'S'),
+        ('M', 'M'),
+        ('L', 'L'),
+        ('XL', 'XL'),
+        ('XXL', 'XXL'),
     )
 
     size = models.CharField(max_length=5, choices=SIZE_CHOICES, blank=True, null=True)
